c4fec5d8198f1f4e3937b79694cd0ee83f82998a
Mark Halls <mark.halls@gmail.com>
2019-12-04 16:57:07 -0700

    Add GET routes for users

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,58 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const validateUserId = require(`../api/middleware/validateUserId`);
+
+const db = require(`./userDb`);
+
+const errorMessage = (res, status, msg, error) => {
+  console.error(msg, error);
+  res.status(status).json({ errorMessage: msg });
+};
+
+router.post("/", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  // do your magic!
+  db.get()
+    .then(users => res.status(200).json(users))
+    .catch(error =>
+      errorMessage(res, 500, `Error getting users from the database`, error)
+    );
+});
+
+router.get("/:id", validateUserId, (req, res) => {
+  // do your magic!
+  res.status(200).json(req.user);
+});
+
+router.get("/:id/posts", validateUserId, (req, res) => {
+  // do your magic!
+  db.getUserPosts(req.user.id)
+    .then(posts => res.status(200).json(posts))
+    .catch(error =>
+      errorMessage(
+        res,
+        500,
+        `Error getting users posts from the database`,
+        error
+      )
+    );
+});
+
+router.delete("/:id", validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.put("/:id", validateUserId, (req, res) => {
   // do your magic!
 });
-
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
-  // do your magic!
-});
-
-//custom middleware
-
-function validateUserId(req, res, next) {
-  // do your magic!
-}
-
-function validateUser(req, res, next) {
-  // do your magic!
-}
-
-function validatePost(req, res, next) {
-  // do your magic!
-}
 
 module.exports = router;
